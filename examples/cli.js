62bd0c3fb97c091987b5ff17a454204a3d30784f
darkbotzmd <darkbotzmd@gmail.com>
2023-04-22 01:56:03 +0700

    .

---
+++ examples/cli.js
@@ -0,0 +1,9 @@
+const QRCode = require('../lib')
+
+QRCode.toString('yo yo yo', function (error, data) {
+  if (error) {
+    throw new Error(error)
+  }
+
+  console.log(data)
+})
---
+++ examples/save.js
@@ -0,0 +1,12 @@
+const QRCode = require('../lib')
+
+const path = './tmp.png'
+QRCode.toFile(path, 'life of the party bros', {
+  color: {
+    dark: '#00F', // Blue modules
+    light: '#0000' // Transparent background
+  }
+}, function (err) {
+  if (err) throw err
+  console.log('saved.')
+})
